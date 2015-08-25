cd187b46e710020790ab03dd8a3a23d073f7ca4f
Mark Anderson <udacity.com@ma7.org>
2015-08-25 10:59:09 -0500

    draft version, still TODOs

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,355 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var work = {
+    "jobs": [
+        {
+            "title": "Computer Science Intern",
+            "dates":" Jun 2015  - Aug 2015",
+            "employer": "Entronix",
+            "description": "Developed Python based web application.",
+            "city": "Kenner, LA"
+        },
+        {
+            "title": "Junior Software Enginneer",
+            "dates": "Oct 2014 - Jun 2015",
+            "employer": "Revelry Labs",
+            "description": "Developed Rails based web applications.",
+            "city": "New Orleans, LA"
+        },
+        {
+            "title": "Developer",
+            "dates": "May 2013  - Aug 2013",
+            "employer": "HaystackEDU",
+            "description": "Developed Rails based web application",
+            "city": "New Orleans, LA"
+        },
+        {
+            "title": "Rails Developer",
+            "dates": "Nov 2011  - Jul 2012",
+            "employer": "iSeatz",
+            "description": "Developed Rails based web application",
+            "city": "New Orleans, LA"
+        },
+        {
+            "title": "Researcher",
+            "dates": "Oct 2010  - Jan 2011",
+            "employer": "Independent Consulting",
+            "description": "Conducted text classification research for Squidoo.",
+            "city": "New Orleans, LA"
+        },
+        {
+            "title": "Public School Teacher",
+            "dates": "Aug 2008  - Jan 2009",
+            "employer": "Recovery School District",
+            "description": "Taught Physical Science",
+            "city": "New Orleans, LA"
+        },
+        {
+            "title": "Chief Scientist",
+            "dates": "1999  - 2006",
+            "employer": "Websense",
+            "description": "Conducted research and development of automated website classification technology using clusters of Linux machines.",
+            "city": "San Diego, CA"
+        },
+        {
+            "title": "Software Engineer",
+            "dates": "1996  - 1999",
+            "employer": "Encyclopaedia Britannica",
+            "description": "Conducted research and developed web based editorial system to review and classify websites for Britannica’s Internet Guide",
+            "city": "San Diego, CA"
+        },
+        {
+            "title": "Assistant Professor",
+            "dates": "1993  - 1996",
+            "employer": "Goucher College",
+            "description": "Taught Mathematics and Computer Science classes including Computer Architecture, Database Systems, and Software Engineering.",
+            "city": "Baltimore, MD"
+        },
+        {
+            "title": "Consultant",
+            "dates": "1991  - 1992",
+            "employer": "San Diego Supercomputer Center",
+            "description": "Provided one-on-one technical support in person, via phone and email, to resolve problems for the users of the center’s supercomputers",
+            "city": "San Diego, CA"
+        },
+        {
+            "title": "User Support and Education Coordinator",
+            "dates": "1989  - 1990",
+            "employer": "Harvey Mudd College",
+            "description": "Hired and trained students technical support staff for the Office of Academic Computing. Provided one-on-one technical support for faculty and staff users of the academic computing facilities.",
+            "city": "Claremont, CA"
+        },
+        {
+            "title": "Software Engineer",
+            "dates": "1988  - 1989",
+            "employer": "Palomar Software",
+            "description": "Designed and wrote custom device driver software for the Macintosh.  Languages used include C, C++, and assembly language.",
+             "city": "Oceanside, CA"
+       },
+        {
+            "title": "Programmer Analyst",
+            "dates": "1987  - 1987",
+            "employer": "University of California, San Diego",
+            "description": "Performed system administration duties and provided user technical support for the Department of Electrical Engineering and Computer Science",
+            "city": "San Diego CA"
+        },
+        {
+            "title": "Programmer Analyst",
+            "dates": "1982  - 1983",
+            "employer": "System Development Corporation",
+            "description": "Designed and wrote compilers for an automated software engineering testing system under a contract with the US Air Force",
+            "city": "Santa Monica, CA"
+        },
+    ]
+};
+
+var projects = {
+    "projects": [
+        {
+            "title": "Fly By Wire -- Towards a Post-Furby metamorphosis",
+            "dates": "Nov 2012 - Dec 2012",
+            "description": "'Fly By Wire -- Towards a Post-Furby metamorphosis' is part of a group show, VOLATILIA, curated my Myrtle von Damitz III, at Barrister's Gallery in New Orleans. 'Fly By Wire' features 1999 era Furbies whose tactile sensor are connected to the internet and who interact with the gallery patrons via twitter and text messaging.",
+            "images": [ "http://placehold.it/300x200" ]
+        },
+        {
+            "title": "Sirens",
+            "dates": "Aug 2011 - Sep 2011",
+            "description": "'Sirens', created by Gumbo Labs, was a installation appearing in ‘A Technological Terrarium,’ curated by Myrtle Von Damitz, at the Ogden Museum of Southern Art.",
+            "images": [ "http://placehold.it/300x200" ]
+        },
+        {
+            "title": "Anternet",
+            "dates": "Apr 2011",
+            "description": "'Anternet' was a Bio-Art piece in the 2011 Automata show. The piece involved tracking the movement of ants through an ant farm and posted messages on a social network in response to their motion.",
+            "images": [ "http://placehold.it/300x200" ]
+        },
+        {
+            "title": "Elastic CD-Rom Twitter Table",
+            "dates": "Jul 2010 - Aug 2010",
+            "description": "The installation was an interactive kinetic sculpture capable of displaying three characters. Each character was formed by seven CD-Rom drives pushing against fabric. It showed the latest tweet containing 'BarCampNOLA' in a side-scrolling fashion, three letters at a time.",
+            "images": [ "http://placehold.it/300x200" ]
+        },
+        {
+            "title": "'Orpheus Descending' -- DesCours 2009",
+            "dates": "Dec 2009",
+            "description": "'Orpheus Descending' by Jimmy Stamp, Sergio Padilla, Frederick Stivers (NO/other) and Gumbo Labs, part of DesCours 2009 Installation series sponsored by the New Orleans Chapter of the American Institute of Architects.",
+            "images": [ "http://placehold.it/300x200" ]
+        }
+    ]
+};
+
+var bio = {
+    "name": "Mark Anderson",
+    "role": "Front End Developer",
+    "contact info": "877-240-5431",
+    "bioPic": "https://dl.dropboxusercontent.com/u/3514030/MarkA.jpg",
+    "welcomeMsg": "Hey, get off my lawn!",
+    "skills": [
+        "banging bits", 
+        "punching cards", 
+        "thinking without boxes",
+        "telling long involved war stories which never reach a destination"
+    ]
+}
+
+var education = {
+    "schools": [
+        {
+            "name": "University of California, San Diego",
+            "city": "San Diego, CA",
+            "degree": "PhD",
+            "dates": "1990 - 1993",
+            "majors": ["Computer Science"]
+        },
+        {
+            "name": "University of California, San Diego",
+            "city": "San Diego, CA",
+            "degree": "MS",
+            "dates": "1983 - 1987",
+            "majors": ["Computer Science"]
+        },
+        {
+            "name": "Harvey Mudd College",
+            "city": "Claremont, CA",
+            "degree": "BS",
+            "dates": "1978 - 1982",
+            "majors": ["Mathematics"]
+        },
+    ],
+    "onlineCourses": [
+        {
+            "title": "CS190.1x Scalable Machine Learning - Edx.org / BerkeleyX",
+            "school": "Edx.org / BerkeleyX",
+            "dates": "Aug 2015",
+            "URL": "https://courses.edx.org/courses/BerkeleyX/CS190.1x/1T2015/info"
+        },
+        {
+            "title": "CS100.1x Introduction to Big Data with Apache Spark",
+            "school": "Edx.org / BerkeleyX",
+            "dates": "Jul 2015",
+            "URL": "https://courses.edx.org/courses/BerkeleyX/CS100.1x/1T2015/info"
+        },
+        {
+            "title": "R Programming",
+            "school": "Coursera / Johns Hopkins University",
+            "dates": "Jun 2015",
+            "URL": "https://www.coursera.org/course/rprog"
+        },
+        {
+            "title": "Developing Data Products",
+            "school": "Coursera / Johns Hopkins University",
+            "dates": "Jun 2015",
+            "URL": "https://www.coursera.org/course/devdataprod"
+        },
+        {
+            "title": "Practical Machine Learning",
+            "school": "Coursera / Johns Hopkins University",
+            "dates": "May 2015",
+            "URL": "https://www.coursera.org/course/predmachlearn"
+        },
+        {
+            "title": "Regression Models",
+            "school": "Coursera / Johns Hopkins University",
+            "dates": "Apr 2015",
+            "URL": "https://www.coursera.org/course/regmods"
+        },
+        {
+            "title": "Statistical Inference",
+            "school": "Coursera / Johns Hopkins University",
+            "dates": "Feb 2015",
+            "URL": "https://www.coursera.org/course/statinference"
+        },
+        {
+            "title": "Exploratory Data Analysis",
+            "school": "Coursera / Johns Hopkins University",
+            "dates": "Feb 2015",
+            "URL": "https://www.coursera.org/course/exdata"
+        },
+        {
+            "title": "Reproducible Research",
+            "school": "Coursera / Johns Hopkins University",
+            "dates": "Feb 2015",
+            "URL": "https://www.coursera.org/course/repdata"
+        },
+        {
+            "title": "Getting and Cleaning Data",
+            "school": "Coursera / Johns Hopkins University",
+            "dates": "Jan 2015",
+            "URL": "https://www.coursera.org/course/getdata"
+        },
+        {
+            "title": "The Data Scientist’s Toolbox",
+            "school": "Coursera / Johns Hopkins University",
+            "dates": "Nov 2014",
+            "URL": "https://www.coursera.org/course/datascitoolbox"
+        }, 
+        {
+            "title": "Web Application Architectures",
+            "school": "Coursera / University of New Mexico",
+            "dates": "Nov 2014",
+            "URL": "https://www.coursera.org/course/webapplications"
+        },
+    ]
+}
+
+function insertInto(helper, replacementString) {
+    return helper.replace("%data%", replacementString)
+}
+
+bio.display = function() {
+    // TODO: contact info
+    $("#header").prepend(insertInto(HTMLwelcomeMsg, bio.welcomeMsg));
+    $("#header").prepend(insertInto(HTMLbioPic, bio.bioPic));
+    $("#header").prepend(insertInto(HTMLheaderRole, bio.role));
+    $("#header").prepend(insertInto(HTMLheaderName, bio.name));
+    if (bio.skills.length >0 ) {
+        $("#header").append(HTMLskillsStart);
+        for (skill in bio.skills) {
+            theSkill = bio.skills[skill];
+
+            $("#skills").append(insertInto(HTMLskills, theSkill));
+        }
+    }
+}
+
+
+work.display = function() {
+    for (job in work.jobs) {
+        var theJob = work.jobs[job];
+
+        $("#workExperience").append(HTMLworkStart);
+
+        var formattedEmployer = insertInto(HTMLworkEmployer, theJob.employer);
+        var formattedTitle = insertInto(HTMLworkTitle, theJob.title);
+        var formattedEmployerTitle = formattedEmployer + formattedTitle;
+        $(".work-entry:last").append(formattedEmployerTitle);
+        $(".work-entry:last").append(insertInto(HTMLworkDates, theJob.dates));
+        $(".work-entry:last").append(insertInto(HTMLworkLocation, theJob.city));
+        $(".work-entry:last").append(insertInto(HTMLworkDescription, theJob.description));
+    }   
+}
+
+projects.display = function() {
+    for (project in projects.projects) {
+        var theProject = projects.projects[project];
+
+        $("#projects").append(HTMLprojectStart);
+
+        $(".project-entry:last").append(insertInto(HTMLprojectTitle, theProject.title));
+        $(".project-entry:last").append(insertInto(HTMLprojectDates, theProject.dates));
+        $(".project-entry:last").append(insertInto(HTMLprojectDescription, theProject.description));
+
+        if (theProject.images.length > 0) {
+            for (image in theProject.images) {
+                var theImage = theProject.images[image];
+
+                $(".project-entry:last").append(insertInto(HTMLprojectImage, theImage));
+            }
+        }
+    }
+}
+
+
+education.display = function() {
+    for (school in education.schools) {
+        var theSchool = education.schools[school];
+
+        $("#education").append(HTMLschoolStart);
+
+        var formattedName = insertInto(HTMLschoolName, theSchool.name);
+        var formattedDegree = insertInto(HTMLschoolDegree, theSchool.degree);
+        formattedNameDegree = formattedName + formattedDegree;
+        $(".education-entry:last").append(formattedNameDegree)
+        $(".education-entry:last").append(insertInto(HTMLschoolDates, theSchool.dates));
+        $(".education-entry:last").append(insertInto(HTMLschoolLocation, theSchool.city))
+        
+        if (theSchool.majors.length > 0) {
+            for (major in theSchool.majors) {
+                var theMajor= theSchool.majors[major];
+
+                $(".education-entry:last").append(insertInto(HTMLschoolMajor, theMajor));
+            }
+        }
+    }
+
+    $("#education").append(HTMLonlineClasses);
+
+    for (course in education.onlineCourses) {
+        theCourse = education.onlineCourses[course];
+
+        $("#education").append(HTMLschoolStart);
+
+        var formattedTitle = insertInto(HTMLonlineTitle, theCourse.title);
+        var formattedSchool = insertInto(HTMLonlineSchool, theCourse.school);
+        var formattedTitleSchool  = formattedTitle + formattedSchool;
+        $(".education-entry:last").append(formattedTitleSchool);
+        $(".education-entry:last").append(insertInto(HTMLonlineDates, theCourse.dates));
+        $(".education-entry:last").append(insertInto(HTMLonlineURL, theCourse.URL));
+    }
+}
+
+bio.display();
+work.display();
+projects.display();
+education.display();
+
+// TODO: get Map working
+$("#mapDiv").append(googleMap);
