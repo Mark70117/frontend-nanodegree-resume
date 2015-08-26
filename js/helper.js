5c6f4b8dfb580ded9cdc84202f323b41545ef2bc
Mark Anderson <udacity.com@ma7.org>
2015-08-26 11:43:19 -0500

    need for step 5 'your resume shoul also console.log() information about click locations

--- js/helper.js
+++ js/helper.js
@@ -69,6 +69,25 @@ $(document).ready(function() {
 
 
 /*
+The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
+*/
+clickLocations = [];
+
+function logClicks(x,y) {
+  clickLocations.push(
+    {
+      x: x,
+      y: y
+    }
+  );
+  console.log('x location: ' + x + '; y location: ' + y);
+}
+
+$(document).click(function(loc) {
+  logClicks(loc.pageX, loc.pageY);
+});
+
+/*
 This is the fun part. Here's where we generate the custom Google Map for the website.
 See the documentation below for more details.
 https://developers.google.com/maps/documentation/javascript/reference
